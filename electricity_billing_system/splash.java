a77954522c1810c74a7faaae49193d7379ac2fb3
Draweshwarkumar <shivamjha22948@gmail.com>
2024-07-13 20:44:11 +0530

    Initial commit

---
+++ electricity_billing_system/splash.java
@@ -0,0 +1,15 @@
+package electricity_billing_system;
+
+import javax.swing.JFrame;
+
+public class splash extends JFrame {
+    splash() {
+        setSize(400, 400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new splash();
+    }
+
+}
